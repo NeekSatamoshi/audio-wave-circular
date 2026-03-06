3d75753bd6fd0cfa37409b3231fc36561334923d
NeekSatamoshi <gabrieltorres.ml30@gmail.com>
2026-03-06 09:14:56 -0300

    Refactor gradient color stops in Circle.js

--- Circle.js
+++ Circle.js
@@ -48,10 +48,7 @@ class Circle {
 				x, y, 0,
 				x, y, this.r * 0.7
 			)
-
-			/* gradient.addColorStop(0, colors[colorIndex])
-			   gradient.addColorStop(0.6, colors[colorIndex])
-			   gradient.addColorStop(1, "transparent") */
+			
 			gradient.addColorStop(0, colors[colorIndex])
 			gradient.addColorStop(0.7, colors[colorIndex])
 			gradient.addColorStop(1, "transparent")
@@ -66,15 +63,3 @@ class Circle {
 		pop()
 	}
 }
-
-/* gradient1.addColorStop(0, "#b3edff")
-gradient1.addColorStop(0.4, "#99dbff")
-gradient1.addColorStop(0.7, "#47ceff")
-gradient1.addColorStop(1, "#2a7bbe") */
-
-/* COLORS SET ROXO= [
-			ctx.fillStyle = "#00CED1"
-			"#8A2BE2",
-			"#000080"
-		] */
-
