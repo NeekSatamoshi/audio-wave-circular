26952d95728de04d6804377ba2d9dca69f22c803
NeekSatamoshi <gabrieltorres.ml30@gmail.com>
2026-03-10 14:11:07 -0300

    CircularGradientV2

--- Circle.js
+++ Circle.js
@@ -1,41 +1,75 @@
 class Circle {
-	constructor(r, n) {
-		this.r = r
-		this.n = n
-		this.offset = 0
-	}
-
-	update() {
-		this.offset += 1.0   // velocidade da animação
+	constructor(r) {
+		this.r = r;
+		this.rotation = random(TWO_PI);
+		this.mic = false;
+		this.time = millis();
+		this.pg = createGraphics(this.r * 2, this.r * 2);
+		this.pg.pixelDensity(1);
 	}
 
 	draw() {
 		let ctx = drawingContext
-		
-		let centerX = windowWidth / 2
-		let centerY = windowHeight / 2
 
-		// movimento interno do gradiente (orbital)
-		let gradientX = centerX + cos(this.offset) * this.r * 0.4
-		let gradientY = centerY + sin(this.offset) * this.r * 0.4
+		push()
+		translate(width / 2, height / 2)
 
-		let gradient = ctx.createRadialGradient(
-			gradientX,
-			gradientY,
-			this.r * 0.1,
-			centerX,
-			centerY,
-			this.r
-		)
+		ctx.save()
 
-		gradient.addColorStop(0, "#b3edff")
-		gradient.addColorStop(0.4, "#99dbff")
-		gradient.addColorStop(0.7, "#47ceff")
-		gradient.addColorStop(1, "#2a7bbe")
+		// máscara circular
+		ctx.beginPath()
+		ctx.arc(0, 0, this.r, 0, Math.PI * 2)
+		ctx.clip()
 
-		ctx.fillStyle = gradient
+		// fundo neutro
+		ctx.fillStyle = "#7492f3"
+		ctx.fillRect(-this.r, -this.r, this.r * 2, this.r * 2)
 
-		noStroke()
-		ellipse(centerX, centerY, this.r * 2)
+		const colors = [
+			"#0077ff",
+			"#47ceff",
+			"#b0e1fb",
+		]
+
+		if (!this.mic) {
+			this.rotation += 0.5;
+		}
+
+		let rotation = this.rotation;
+
+		ctx.globalCompositeOperation = "source-over"
+		ctx.filter = "blur(35px)"
+
+		let totalBlobs = 6;
+
+
+		for (let i = 1; i <= totalBlobs; i++) {
+			let angle = rotation + i * (360 / totalBlobs)
+
+			let radius = this.r * 0.60
+
+			let x = cos(angle) * radius
+			let y = sin(angle) * radius
+
+			let gradient = ctx.createRadialGradient(
+				x, y, 0,
+				x, y, this.r / 2
+			)
+
+			let colorIndex = (i - 1) % colors.length
+
+			gradient.addColorStop(0, colors[colorIndex])
+			gradient.addColorStop(1, colors[colorIndex])
+			gradient.addColorStop(1, "transparent")
+
+			noStroke()
+			ctx.fillStyle = gradient
+			ctx.fillRect(-this.r * 2, -this.r * 2, this.r * 4, this.r * 4)
+		}
+
+		ctx.filter = "none"
+
+		ctx.restore()
+		pop()
 	}
 }
